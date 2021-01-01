889e6676a87cd906256c8c5d1302d2bc98a0c974
DT Pham <dt99.pham@gmail.com>
2021-01-01 12:38:42 -0800

    Renamed game's View.

--- Set-Game/SetGameView.swift
+++ Set-Game/SetGameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  SetGameView.swift
 //  Set-Game
 //
 //  Created by Duong Pham on 1/1/21.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SetGameView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        SetGameView()
     }
 }
