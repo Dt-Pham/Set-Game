815064d9921289a68784af03752995485c4dc120
DT Pham <dt99.pham@gmail.com>
2021-01-01 11:11:55 -0800

    Initial Commit

---
+++ Set-Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set-Game
+//
+//  Created by Duong Pham on 1/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
